e05fa0555812dd3d5d8389e2b3f80c621b73e638
Vytautas.Miseikis <tedyvm@gmail.com>
2025-03-28 13:17:44 +0200

    03-28

--- anim.js
+++ anim.js
@@ -10,37 +10,39 @@ window.addEventListener("scroll", () => {
     if (yOffset > 135) {
       document.querySelector(".button-2").style.display = "block";
 
-      const x = (yOffset - 135) * 2 + 102;
-      if (x <= 170) {
+      const x = (yOffset - 135) * 2 + 62;
+      if (x <= 130) {
         document.querySelector(".button-2").style.top = `${x}px`;
       } else {
-        document.querySelector(".button-2").style.top = "170px";
+        document.querySelector(".button-2").style.top = "130px";
       }
     } else {
       document.querySelector(".button-2").style.display = "none";
     }
     document.querySelector(".s1img").style.left = `${yOffset / 1.5 - 250}px`;
     document.querySelector(".s2img").style.left = `${-(yOffset / 1.5) + 100}px`;
-    document.querySelector(".scrollBg").style.backgroundPosition = `calc(50% + ${300 - yOffset}px) 50%`;
-    document.querySelector(".s4-img1").style.transform = `rotate(${(yOffset - 450) / 7}deg)`;
-    document.querySelector(".s4-img2").style.paddingLeft = `${200-(yOffset - 400)}px`;
+    document.querySelector(".s4-img2").style.left = `${(yOffset - 390) / 0.7}px`;
+    document.querySelector(".scrollBg").style.backgroundPosition = `calc(50% + ${50 - yOffset}px) 50%`;
+    // document.querySelector(".s4-img1").style.transform = `rotate(${(yOffset - 450) / 7}deg)`;
+    // document.querySelector(".s4-img2").style.paddingLeft = `${200-(yOffset - 400)}px`;
   }
 
   // SM ekranui
-  else {
-    if (yOffset > 107) {
-      document.querySelector(".button-2").style.display = "block";
-      const x = (yOffset - 107) * 2 + 52;
-      if (x <= 120) {
-        document.querySelector(".button-2").style.top = `${x}px`;
-      } else {
-        document.querySelector(".button-2").style.top = "120px";
-      }
-    } else {
-      document.querySelector(".button-2").style.display = "none";
-    }
-    document.querySelector(".s1img").style.left = `${yOffset / 2 - 150}px`;
-    document.querySelector(".s2img").style.left = `${-(yOffset / 2) + 100}px`;
-    document.querySelector(".scrollBg").style.backgroundPosition = `calc(50% + ${120 - yOffset}px) 50%`;
-  }
+  // else {
+  //   if (yOffset > 107) {
+  //     document.querySelector(".button-2").style.display = "block";
+  //     const x = (yOffset - 107) * 2 + 52;
+  //     if (x <= 120) {
+  //       document.querySelector(".button-2").style.top = `${x}px`;
+  //     } else {
+  //       document.querySelector(".button-2").style.top = "120px";
+  //     }
+  //   } else {
+  //     document.querySelector(".button-2").style.display = "none";
+  //   }
+  //   document.querySelector(".s1img").style.left = `${yOffset / 2 - 150}px`;
+  //   document.querySelector(".s2img").style.left = `${-(yOffset / 2) + 100}px`;
+  //   document.querySelector(".scrollBg").style.backgroundPosition = `calc(50% + ${-40 - yOffset}px) 50%`;
+  //   document.querySelector(".s4-img2").style.left = `${(yOffset - 520) / 0.7}px`;
+  // }
 });
