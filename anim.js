ea15af1ae8484cb399e4326bde5d660e6e6d6fa7
Vytautas.Miseikis <tedyvm@gmail.com>
2025-04-01 22:07:41 +0300

    04-01_landinPage baigtas

--- anim.js
+++ anim.js
@@ -10,17 +10,17 @@ window.addEventListener("scroll", () => {
     if (yOffset > 135) {
       document.querySelector(".button-2").style.display = "block";
 
-      const x = (yOffset - 135) * 2 + 62;
-      if (x <= 130) {
-        document.querySelector(".button-2").style.top = `${x}px`;
+      const x = yOffset - 135 - 156 - 68;
+      if (x <= -156) {
+        document.querySelector(".button-2").style.left = `${x}px`;
       } else {
-        document.querySelector(".button-2").style.top = "130px";
+        document.querySelector(".button-2").style.left = "-156px";
       }
     } else {
       document.querySelector(".button-2").style.display = "none";
     }
-    document.querySelector(".s1img").style.left = `${yOffset / 1.5 - 250}px`;
-    document.querySelector(".s2img").style.left = `${-(yOffset / 1.5) + 100}px`;
+    document.querySelector(".s1img").style.left = `${yOffset / 2 - 250}px`;
+    document.querySelector(".s2img").style.left = `${-(yOffset / 2) + 100}px`;
     document.querySelector(".s4-img2").style.left = `${(yOffset - 390) / 0.7}px`;
     document.querySelector(".scrollBg").style.backgroundPosition = `calc(50% + ${50 - yOffset}px) 50%`;
     // document.querySelector(".s4-img1").style.transform = `rotate(${(yOffset - 450) / 7}deg)`;
@@ -29,17 +29,17 @@ window.addEventListener("scroll", () => {
 
   // SM ekranui
   else {
-    // if (yOffset > 107) {
-    //   document.querySelector(".button-2").style.display = "block";
-    //   const x = (yOffset - 107) * 2 + 52;
-    //   if (x <= 120) {
-    //     document.querySelector(".button-2").style.top = `${x}px`;
-    //   } else {
-    //     document.querySelector(".button-2").style.top = "120px";
-    //   }
-    // } else {
-    //   document.querySelector(".button-2").style.display = "none";
-    // }
+    if (yOffset > 107) {
+      document.querySelector(".button-2").style.display = "block";
+      const x = yOffset - 105 - 120 - 36;
+      if (x <= -102) {
+        document.querySelector(".button-2").style.left = `${x}px`;
+      } else {
+        document.querySelector(".button-2").style.left = "-102px";
+      }
+    } else {
+      document.querySelector(".button-2").style.display = "none";
+    }
     document.querySelector(".s1img").style.left = `${yOffset / 2 - 150}px`;
     document.querySelector(".s2img").style.left = `${-(yOffset / 2) + 100}px`;
     document.querySelector(".scrollBg").style.backgroundPosition = `calc(50% + ${-40 - yOffset}px) 50%`;
